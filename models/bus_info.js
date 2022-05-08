edeb0c8ddc88dd11d3809e3e7324701392a1cb1b
huijae <mafufan@naver.com>
2022-05-08 15:24:06 +0900

    Add Bulletin and Comment Tables, Modify  Models code

--- models/bus_info.js
+++ models/bus_info.js
@@ -1,38 +1,39 @@
-const Sequelize = require('sequelize');
-module.exports = class User extends Sequelize.Model {
+const Sequelize = require("sequelize");
+module.exports = class BusInfo extends Sequelize.Model {
   static init(sequelize) {
     return super.init(
       {
         bus_id: {
-          primaryKey: true,
           type: Sequelize.INTEGER,
           allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
         },
         type: {
           type: Sequelize.STRING(30),
-          allowNull: false,
+          allowNull: false
         },
-        stop_stop: {
+        bus_stop: {
           type: Sequelize.STRING(30),
-          allowNull: false,
-        },
+          allowNull: false
+        }
       },
       {
         sequelize,
         timestamps: false,
         underscored: false,
-        modelName: 'BusInfo',
-        tableName: 'bus_info',
+        modelName: "BusInfo",
+        tableName: "bus_info",
         paranoid: false,
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci',
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci"
       }
     );
   }
   static associate(db) {
     db.BusInfo.belongsTo(db.AdmInfo, {
-      foreignKey: 'adm_id',
-      sourceKey: 'adm_id',
+      foreignKey: "adm_id",
+      sourceKey: "adm_id"
     });
   }
 };
