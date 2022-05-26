ee5595ff3c465e03089fd2577eb6c3f58a4f187a
huijae <mafufan@naver.com>
2022-05-27 02:34:14 +0900

    AsReq React 테스트

--- src/App.js
+++ src/App.js
@@ -1,48 +1,29 @@
-import axios from 'axios';
+/*eslint-disable*/
 import React, { useEffect, useState } from 'react';
 import Menu from './components/Menu';
-import Login from './components/Login';
 import Header from './components/JHeader';
 import Main from './components/Main';
-import Calendar from './components/Calender';
-
 
 function App(props) {
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
   const [mode, setMode] = useState('Login');
 
   useEffect(() => {
     console.log(mode);
   }, [mode]);
 
-  function clickButton(e) {
-    // 로그인 버튼 클릭
-    e.preventDefault();
-    setIsLogin(true);
-    setMode('ManagerPage');
-  }
-
-  const onChangeMode = (mode) => {
+  const onChangeMode = mode => {
     // 메뉴 항목 클릭
     setMode(mode);
   };
 
-  if (!isLogin) {
-    return (
-      <div className='containerL'>
-        <Login button={clickButton} />
-      </div>
-    );
-  } else { 
-    return (
-      <div className='containerM'>
-        <Header mode={mode} />
-        <Menu onChangeMode={onChangeMode}></Menu>
-        <Main mode={mode} />
-
-      </div>
-    );
-  }
+  return (
+    <div className="containerM">
+      <Header mode={mode} />
+      <Menu onChangeMode={onChangeMode}></Menu>
+      <Main mode={mode} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Main/AsRequest.js
+++ src/components/Main/AsRequest.js
@@ -1,198 +1,173 @@
+/*eslint-disable*/
 /* import '../Css/Main.css'; */
-import "../Css/JMain.css";
-import { DatePicker } from "react-datepicker";
-import "react-date-range/dist/styles.css"; // main style file
-import "react-date-range/dist/theme/default.css"; // theme css file
-import { useState } from "react";
+import '../Css/JMain.css';
+import { DatePicker } from 'react-datepicker';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function AsRequest() {
-  const [firstday, setFirstday] = useState();
-  const [secondday, setSecondday] = useState();
-   console.log(firstday);
-  console.log(secondday); 
-  /* console.log((document.getElementById('dateF').value)); */
+  const [data, setData] = useState('');
+  const [stdId, setStdId] = useState('');
+  const [stdName, setStdName] = useState('');
+  const [vstCheck, setVstCheck] = useState('');
+  const [confirm, setConfirm] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+
+  const callBack = () => {
+    vstCheck === '동의' ? true : false;
+    axios
+      .post('http://localhost:3001/admin/as', {
+        std_id: stdId,
+        std_name: stdName,
+        start_date: startDate,
+        end_date: endDate,
+        vst_check: vstCheck,
+        confirm: confirm,
+      })
+      .then(res => {
+        console.log(res.data);
+        console.log('성공');
+        setData(res.data);
+      })
+      .catch(err => {
+        console.log('정보 없는데요');
+      });
+  };
+
+  useEffect(() => {
+    callBack();
+  }, []);
   return (
     <div className="mainBody">
-        <fieldset className="field">
-          <legend>검색</legend>
-          <input
+      <fieldset className="field">
+        <legend>검색</legend>
+        <input
           className="inputA"
-            type="date"
-            name="dateF"
-            id="dateF"
-            onChange={(e) => setFirstday(e.target.value)}
+          type="date"
+          name="dateF"
+          id="dateF"
+          onChange={e => setStartDate(e.target.value)}
+        />
+        ~
+        <input
+          className="inputA"
+          type="date"
+          name="dateS"
+          onChange={e => setEndDate(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            callBack();
+          }}
+          className="buttonStyle"
+          type="submit"
+        >
+          조회
+        </button>
+        <div style={{ margin: 10 }}>
+          <label>학번 : </label>
+          <input
+            onChange={e => {
+              setStdId(e.target.value);
+            }}
+            className="inputB"
+            type="text"
+            style={{ width: 60 }}
           />
-          ~
+          <label> 이름 : </label>
           <input
-          className="inputA"
-            type="date"
-            name="dateS"
-            onChange={(e) => setSecondday(e.target.value)}
+            onChange={e => {
+              setStdName(e.target.value);
+            }}
+            className="inputB"
+            type="text"
+            style={{ width: 60 }}
+          />
+          <label> 부재시방문 : </label>
+          <input
+            onChange={e => {
+              setVstCheck(e.target.value);
+            }}
+            className="inputB"
+            type="text"
+            style={{ width: 60 }}
+          />
+          <label> 상태 : </label>
+          <input
+            onChange={e => {
+              setConfirm(e.target.value);
+            }}
+            className="inputB"
+            type="text"
+            style={{ width: 60 }}
           />
           <button
-            className="buttonStyle"
-            type="submit" 
-          >
-            조회
-          </button>
-          <div style={{ margin: 10 }}>
-            <label>학번 : </label>
-            <input className="inputB" type="text" style={{ width: 60 }} />
-            <label> 이름 : </label>
-            <input className="inputB" type="text" style={{ width: 60 }} />
-            <label> 상태 : </label>
-            <input className="inputB" type="text" style={{ width: 60 }} />
-            <button
+            onClick={() => {
+              callBack();
+            }}
             className="buttonStyle"
             type="submit"
-            
           >
             조회
           </button>
-          </div>
-        </fieldset>
+        </div>
+      </fieldset>
       <div className="tableBox">
         <table className="tableH">
-      <thead>
-    <tr className="trStyle">
-      <th>번호</th>
-      <th>학번</th>
-      <th>이름</th>
-      <th>호실</th>
-      <th>내용</th>
-      <th>전화번호</th>
-      <th>신청날짜</th>
-      <th>상태</th>
-      <th>확인</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr style={{ height: '4vh', backgroundColor: '#e9e9e9' }}>
-      <td>1</td>
-      <td>2101032</td>
-      <td>김동구</td>
-      <td>704</td>
-      <td>보일러 고장</td>
-      <td>010-4773-4114</td>
-      <td>2022.05.02</td>
-      <td>미확인</td>
-    </tr>
-  </tbody>
-  </table>
-  </div>
+          <thead>
+            <tr className="trStyle">
+              <th>번호</th>
+              <th>학번</th>
+              <th>이름</th>
+              <th>호실</th>
+              <th>내용</th>
+              <th>전화번호</th>
+              <th>신청날짜</th>
+              <th>부재시방문</th>
+              <th>확인</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data != false
+              ? data.map(function (data, i) {
+                  return (
+                    <Table key={i} data={data} i={i} />
+                  );
+                })
+              : null}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
 
+function Table({ i, data }) {
+  return (
+    <>
+      <tr
+        key={i}
+        style={{
+          height: '4vh',
+          backgroundColor: '#e9e9e9',
+        }}
+      >
+        <td>{data.as_id}</td>
+        <td>{data.std_id}</td>
+        <td>{data.StdInfo.std_name}</td>
+        <td>{data.StdInfo.room_num}</td>
+        <td>{data.title}</td>
+        <td>{data.StdInfo.ph_num}</td>
+        <td>{data.request_date}</td>
+        <td>{data.vst_check === true ? '동의' : '거부'}</td>
+        <td>
+          {data.confirm === true ? '처리완료' : '미처리'}
+        </td>
+      </tr>
+    </>
+  );
+}
 export default AsRequest;
-
-/* <form style={{ display: 'flex', justifyContent: 'center' }}>
-<div style={{ display: 'flex', flexDirection: 'column' }}>
-  <div style={{ margin: 10 }}>
-    <label>기간 : </label>
-    <input type='text' style={{ width: 60 }} />
-    <label> 년 </label>
-    <input type='text' style={{ width: 30 }} />
-    <label> 월 </label>
-    <input type='text' style={{ width: 30 }} />
-    <label> 일 ~ </label>
-    <input type='text' style={{ width: 60 }} />
-    <label> 년 </label>
-    <input type='text' style={{ width: 30 }} />
-    <label> 월 </label>
-    <input type='text' style={{ width: 30 }} />
-    <label> 일 </label>
-  </div>
-  <div style={{ margin: 10 }}>
-    <label>학번 : </label>
-    <input type='text' style={{ width: 60 }} />
-    <label> 이름 : </label>
-    <input type='text' style={{ width: 60 }} />
-    <label> 상태 : </label>
-    <input type='text' style={{ width: 60 }} />
-  </div>
-</div>
-<div
-  style={{
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-  }}
->
-  <button
-    style={{
-      backgroundColor: 'orange',
-      color: 'white',
-      borderStyle: 'none',
-      borderRadius: '5px',
-      paddingBlock: '3px',
-      paddingInline: '10px',
-      marginLeft: '15px',
-      marginTop:  '53px',
-    }}
-  >
-    조회
-  </button>
-</div>
-</form>
-<form>
-<table
-  className='tableSet'
-  style={{
-    borderCollapse: 'collapse',
-    width: '100%',
-    textAlign:'center'
-  }}
->
-  <thead>
-    <tr
-      style={{
-        color: 'white',
-        backgroundColor: '#21429c',
-        height: '4vh',
-      }}
-    >
-      <td>번호</td>
-      <td>학번</td>
-      <td>이름</td>
-      <td>호실</td>
-      <td>내용</td>
-      <td>전화번호</td>
-      <td>신청날짜</td>
-      <td>상태</td>
-      <td>확인</td>
-    </tr>
-  </thead>
-  <tbody>
-    <tr style={{ height: '4vh', backgroundColor: '#e9e9e9' }}>
-      <td>1</td>
-      <td>2101032</td>
-      <td>김동구</td>
-      <td>704</td>
-      <td>보일러 고장</td>
-      <td>010-4773-4114</td>
-      <td>2022.05.02</td>
-      <td>미확인</td>
-      <td>
-        <input type='checkbox' />
-      </td>
-    </tr>
-  </tbody>
-</table>
-<button
-  style={{
-    float: 'right',
-    width: 130,
-    backgroundColor: 'orange',
-    color: 'white',
-    borderStyle: 'none',
-    borderRadius: '5px',
-
-    paddingBlock: '3px',
-    paddingInline: '10px',
-    margin: 5,
-  }}
->
-  적용
-</button>
-</form> */
